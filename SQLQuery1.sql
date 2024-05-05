518858e042a44decd45c857936608c02d278a156
xacomen <minhhieunguyentrong@gmail.com>
2024-05-05 23:13:18 +0700

    done

---
+++ SQLQuery1.sql
@@ -0,0 +1,459 @@
+﻿create database QhShopV1
+go
+USE [QhShopV1]
+GO
+/****** Object:  Table [dbo].[Accounts]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Accounts](
+	[Username] [varchar](50) NOT NULL,
+	[Password] [varchar](20) NULL,
+	[Fullname] [nvarchar](50) NULL,
+	[Email] [nvarchar](50) NULL,
+	[Photo] [nvarchar](50) NULL,
+	[Phone] [nvarchar](50) NULL,
+	[Active] [bit] NULL,
+ CONSTRAINT [PK_Account] PRIMARY KEY CLUSTERED 
+(
+	[Username] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Authorities]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Authorities](
+	[Authorize_id] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [varchar](50) NULL,
+	[Role_id] [varchar](10) NULL,
+ CONSTRAINT [PK_Authorize] PRIMARY KEY CLUSTERED 
+(
+	[Authorize_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Banners]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Banners](
+	[Banner_id] [int] NULL,
+	[Image] [varchar](50) NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Categories]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Categories](
+	[Category_id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Detail] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED 
+(
+	[Category_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+------------------------------
+-- mate
+CREATE TABLE [dbo].[Material](
+    [Material_id] [int] IDENTITY(1,1) NOT NULL,
+     	[Name] [nvarchar](50) NULL,
+    [Detail] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Material] PRIMARY KEY CLUSTERED 
+(
+    [Material_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+
+-- color
+CREATE TABLE [dbo].[Color](
+    [Color_id] [int] IDENTITY(1,1) NOT NULL,
+     	[Name] [nvarchar](50) NULL,
+    [Detail] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Color] PRIMARY KEY CLUSTERED 
+(
+    [Color_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+-- size
+CREATE TABLE [dbo].[Size](
+    [Size_id] [int] IDENTITY(1,1) NOT NULL,
+     	[Name] [nvarchar](50) NULL,
+    [Detail] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Size] PRIMARY KEY CLUSTERED 
+(
+    [Size_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+/****** Object:  Table [dbo].[Comments]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Comments](
+	[Comment_id] [int] IDENTITY(1,1) NOT NULL,
+	[Product_id] [int] NOT NULL,
+	[Username] [varchar](50) NOT NULL,
+	[Comment_Content] [nvarchar](max) NULL,
+	[Comment_Date] [datetime] NULL,
+	[Image] [varchar](50) NULL,
+ CONSTRAINT [PK_Comments] PRIMARY KEY CLUSTERED 
+(
+	[Comment_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+/****** Object:  Table [dbo].[Favorites]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Favorites](
+	[Favorite_id] [int] IDENTITY(1,1) NOT NULL,
+	[Favorite_date] [date] NOT NULL,
+	[Product_id] [int] NOT NULL,
+	[Username] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Favorites] PRIMARY KEY CLUSTERED 
+(
+	[Favorite_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[image_product]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[image_product](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[ProductID] [int] NULL,
+	[path] [nvarchar](100) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[OrderDetails]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[OrderDetails](
+	[OrderDetail_id] [int] IDENTITY(1,1) NOT NULL,
+	[Order_id] [int] NULL,
+	[Product_id] [int] NULL,
+	[Price] [float] NULL,
+	[Quantity] [int] NULL,
+ CONSTRAINT [PK_OrderDetail] PRIMARY KEY CLUSTERED 
+(
+	[OrderDetail_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Orders]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Orders](
+	[Order_id] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [varchar](50) NULL,
+	[CreateDate] [datetime] NULL,
+	[Address] [nvarchar](100) NULL,
+	[Status] [int] NULL,
+	[Phone] [char](11) NULL,
+	[Description] [nvarchar](50) NULL,
+	[Intent] [nvarchar](50) NULL,
+	[Method] [nvarchar](50) NULL,
+	[Currency] [nvarchar](50) NULL,
+	[Price] [float] NULL,
+	[money_give] [float] NULL,
+	[money_send] [float] NULL,
+	[voucher_price] [float] NULL,
+	[reason] [ntext] NULL,
+	[Card_id] [nvarchar](12) NULL,
+ CONSTRAINT [PK_Order] PRIMARY KEY CLUSTERED 
+(
+	[Order_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Posts]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Posts](
+	[Post_id] [int] IDENTITY(1,1) NOT NULL,
+	[Post_Name] [nvarchar](max) NULL,
+	[Image] [varchar](max) NULL,
+	[Detail] [nvarchar](max) NULL,
+	[Status] [bit] NULL,
+	[Post_date] [datetime] NULL,
+	[Post_conten] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Posts] PRIMARY KEY CLUSTERED 
+(
+	[Post_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[product_detail]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[product_detail](
+	[DetailID] [int] IDENTITY(1,1) NOT NULL,
+	[ProductID] [int] NULL,
+	[Description] [nvarchar](max) NULL,
+	[Detail] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[DetailID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Products]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Products](
+	[Product_id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Unit_price] [float] NULL,
+	[Quantity] [int] NULL,
+	[Product_date] [date] NULL,
+	[Category_id] [int] NOT NULL,
+	[Material_id] [int] NOT NULL,
+	[Size_id] [int] NOT NULL,
+	[Color_id] [int] NOT NULL,
+	[Distcount] [float] NULL,
+	[Special] [bit] NULL,
+	[Lastest] [bit] NULL,
+	[Status] [bit] NULL,
+	[Trademark_id] [int] NOT NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[Product_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Roles]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Roles](
+	[Role_id] [varchar](10) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED 
+(
+	[Role_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Trademarks]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Trademarks](
+	[Trademark_id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Detail] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Trademarks] PRIMARY KEY CLUSTERED 
+(
+	[Trademark_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Votes]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Votes](
+	[Vote_id] [int] IDENTITY(1,1) NOT NULL,
+	[Product_id] [int] NOT NULL,
+	[Username] [varchar](50) NOT NULL,
+	[Vote_Date] [datetime] NULL,
+	[Vote] [int] NULL,
+	[Image] [varchar](50) NULL,
+	[Vote_content] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Votes] PRIMARY KEY CLUSTERED 
+(
+	[Vote_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[voucher_details]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[voucher_details](
+	[Voucher_detail_id] [int] IDENTITY(1,1) NOT NULL,
+	[Voucher_id] [int] NULL,
+	[Order_id] [int] NULL,
+ CONSTRAINT [PK_VoucherDetails] PRIMARY KEY CLUSTERED 
+(
+	[Voucher_detail_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Vouchers]    Script Date: 11/01/2024 12:52:28 SA ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Vouchers](
+	[Voucher_id] [int] IDENTITY(1,1) NOT NULL,
+	[Voucher_name] [varchar](50) NULL,
+	[CreateDate] [datetime] NULL,
+	[EndDate] [datetime] NULL,
+	[Voucher_price] [float] NULL,
+	[Voucher_content] [nvarchar](max) NULL,
+	[Status] [bit] NULL,
+	[Username] [varchar](50) NULL,
+	[estimate] [float] NULL,
+ CONSTRAINT [PK_Vouchers] PRIMARY KEY CLUSTERED 
+(
+	[Voucher_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+INSERT [dbo].[Accounts] ([Username], [Password], [Fullname], [Email], [Photo], [Phone], [Active]) VALUES (N'admin', N'123456', N'ninh văn phước', N'phuchungngo730@gmail.com', N'canbo.png', N'0123456789', 1)
+SET IDENTITY_INSERT [dbo].[Authorities] ON 
+SET IDENTITY_INSERT QhShopV1.dbo.product_detail OFF;
+
+INSERT [dbo].[Authorities] ([Authorize_id], [Username], [Role_id]) VALUES (1, N'admin', N'DIRE')
+SET IDENTITY_INSERT [dbo].[Authorities] OFF
+GO
+
+ALTER TABLE [dbo].[Authorities]  WITH CHECK ADD  CONSTRAINT [FK_Authorize_Account] FOREIGN KEY([Username])
+REFERENCES [dbo].[Accounts] ([Username])
+GO
+ALTER TABLE [dbo].[Authorities] CHECK CONSTRAINT [FK_Authorize_Account]
+GO
+ALTER TABLE [dbo].[Authorities]  WITH CHECK ADD  CONSTRAINT [FK_Authorize_Role] FOREIGN KEY([Role_id])
+REFERENCES [dbo].[Roles] ([Role_id])
+GO
+ALTER TABLE [dbo].[Authorities] CHECK CONSTRAINT [FK_Authorize_Role]
+GO
+ALTER TABLE [dbo].[Comments]  WITH CHECK ADD  CONSTRAINT [FK_Comments_Accounts] FOREIGN KEY([Username])
+REFERENCES [dbo].[Accounts] ([Username])
+GO
+ALTER TABLE [dbo].[Comments] CHECK CONSTRAINT [FK_Comments_Accounts]
+GO
+ALTER TABLE [dbo].[Comments]  WITH CHECK ADD  CONSTRAINT [FK_Comments_Products] FOREIGN KEY([Product_id])
+REFERENCES [dbo].[Products] ([Product_id])
+GO
+ALTER TABLE [dbo].[Comments] CHECK CONSTRAINT [FK_Comments_Products]
+GO
+ALTER TABLE [dbo].[Favorites]  WITH CHECK ADD  CONSTRAINT [FK_Favorites_Accounts] FOREIGN KEY([Username])
+REFERENCES [dbo].[Accounts] ([Username])
+GO
+ALTER TABLE [dbo].[Favorites] CHECK CONSTRAINT [FK_Favorites_Accounts]
+GO
+ALTER TABLE [dbo].[Favorites]  WITH CHECK ADD  CONSTRAINT [FK_Favorites_Products] FOREIGN KEY([Product_id])
+REFERENCES [dbo].[Products] ([Product_id])
+GO
+ALTER TABLE [dbo].[Favorites] CHECK CONSTRAINT [FK_Favorites_Products]
+GO
+ALTER TABLE [dbo].[image_product]  WITH CHECK ADD FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Products] ([Product_id])
+GO
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetail_Order] FOREIGN KEY([Order_id])
+REFERENCES [dbo].[Orders] ([Order_id])
+GO
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetail_Order]
+GO
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetail_Product] FOREIGN KEY([Product_id])
+REFERENCES [dbo].[Products] ([Product_id])
+GO
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetail_Product]
+GO
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Order_Account] FOREIGN KEY([Username])
+REFERENCES [dbo].[Accounts] ([Username])
+GO
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Order_Account]
+GO
+ALTER TABLE [dbo].[product_detail]  WITH CHECK ADD FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Products] ([Product_id])
+GO
+ALTER TABLE [dbo].[Products]  WITH CHECK ADD  CONSTRAINT [FK_Product_Category] FOREIGN KEY([Category_id])
+REFERENCES [dbo].[Categories] ([Category_id])
+GO
+ALTER TABLE [dbo].[Products] CHECK CONSTRAINT [FK_Product_Category]
+GO
+ALTER TABLE [dbo].[Products]  WITH CHECK ADD  CONSTRAINT [FK_Products_Trademarks] FOREIGN KEY([Trademark_id])
+REFERENCES [dbo].[Trademarks] ([Trademark_id])
+GO
+ALTER TABLE [dbo].[Products] CHECK CONSTRAINT [FK_Products_Trademarks]
+GO
+ALTER TABLE [dbo].[Votes]  WITH CHECK ADD  CONSTRAINT [FK_Votes_Accounts] FOREIGN KEY([Username])
+REFERENCES [dbo].[Accounts] ([Username])
+GO
+ALTER TABLE [dbo].[Votes] CHECK CONSTRAINT [FK_Votes_Accounts]
+GO
+ALTER TABLE [dbo].[Votes]  WITH CHECK ADD  CONSTRAINT [FK_Votes_Products] FOREIGN KEY([Product_id])
+REFERENCES [dbo].[Products] ([Product_id])
+GO
+ALTER TABLE [dbo].[Votes] CHECK CONSTRAINT [FK_Votes_Products]
+GO
+ALTER TABLE [dbo].[voucher_details]  WITH CHECK ADD  CONSTRAINT [FK_VoucherDetails_Orders] FOREIGN KEY([Order_id])
+REFERENCES [dbo].[Orders] ([Order_id])
+GO
+ALTER TABLE [dbo].[voucher_details] CHECK CONSTRAINT [FK_VoucherDetails_Orders]
+GO
+ALTER TABLE [dbo].[voucher_details]  WITH CHECK ADD  CONSTRAINT [FK_VoucherDetails_Vouchers] FOREIGN KEY([Voucher_id])
+REFERENCES [dbo].[Vouchers] ([Voucher_id])
+GO
+ALTER TABLE [dbo].[voucher_details] CHECK CONSTRAINT [FK_VoucherDetails_Vouchers]
+GO
+ALTER TABLE [dbo].[Vouchers]  WITH CHECK ADD  CONSTRAINT [FK_Vouchers_Accounts] FOREIGN KEY([Username])
+REFERENCES [dbo].[Accounts] ([Username])
+GO
+ALTER TABLE [dbo].[Vouchers] CHECK CONSTRAINT [FK_Vouchers_Accounts]
+GO
+
+------- [Color]
+ALTER TABLE [dbo].[Products] WITH CHECK 
+ADD CONSTRAINT [FK_Product_Color] FOREIGN KEY([Color_id])
+REFERENCES [dbo].[Color] ([Color_id]
+-----[Material]
+ALTER TABLE [dbo].[Products] WITH CHECK 
+ADD CONSTRAINT [FK_Product_Material] FOREIGN KEY([Material_id])
+REFERENCES [dbo].[Material] ([Material_id]);
+
+----- [Size]
+ALTER TABLE [dbo].[Products] WITH CHECK 
+ADD CONSTRAINT [FK_Product_Size] FOREIGN KEY([Size_id])
+REFERENCES [dbo].[Size] ([Size_id]);
+
+--[Categories]
+ALTER TABLE [dbo].[Products]  WITH CHECK ADD  CONSTRAINT [FK_Product_Category] FOREIGN KEY([Category_id])
+REFERENCES [dbo].[Categories] ([Category_id])
